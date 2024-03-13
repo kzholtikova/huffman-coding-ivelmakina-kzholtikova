7663ef31babb6157b315c853c3f7aef9395187ba
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2024-03-13 23:31:17 +0200

    created main method
    -added filepath and read the file
    -created dict to store symbols and their frequencies

--- AssignmentFour/AssignmentFour/Program.cs
+++ AssignmentFour/AssignmentFour/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿namespace Assignment4;
+
+static class Program
+{
+    public static void Main()
+    {
+        string filePath = "sherlock.txt";
+
+        if (File.Exists(filePath))
+        {
+            string content = File.ReadAllText(filePath);
+
+            Dictionary<char, int> symbolFrequency = new Dictionary<char, int>();
+        } else { Console.WriteLine("File was not found."); }
+    }
+    
+}
