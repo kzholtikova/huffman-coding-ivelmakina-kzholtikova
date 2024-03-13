7cfd276a341d389368f1703fadbe286b7c6d1a50
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2024-03-13 23:32:47 +0200

    CountSymbolFrequency method
    -created method count symbols' frequencies using dict

--- AssignmentFour/AssignmentFour/Program.cs
+++ AssignmentFour/AssignmentFour/Program.cs
@@ -11,7 +11,19 @@ static class Program
             string content = File.ReadAllText(filePath);
 
             Dictionary<char, int> symbolFrequency = new Dictionary<char, int>();
+            CountSymbolFrequency(content, symbolFrequency);
         } else { Console.WriteLine("File was not found."); }
     }
-    
+
+    static void CountSymbolFrequency(string text, Dictionary<char, int> frequency)
+    {
+        foreach (char symbol in text)
+        {
+            if (frequency.ContainsKey(symbol))
+            {
+                frequency[symbol]++;
+            } else { frequency[symbol] = 1; }
+        }
+
+    }
 }
