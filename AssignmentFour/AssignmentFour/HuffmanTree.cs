9fb91a45000cf8256fe98291b57dd70ef484b503
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2024-03-16 17:12:46 +0200

    Build Tree method
    -added BuildTree mehod() that creates a min priority queue containing data-frequency nodes
    -added tree construction logic by repeatedly dequeuing and merging nodes from the priority queue until only one node remains

--- AssignmentFour/AssignmentFour/HuffmanTree.cs
+++ AssignmentFour/AssignmentFour/HuffmanTree.cs
@@ -3,4 +3,24 @@ namespace Assignment4;
 public class HuffmanTree
 {
     private PriorityQueue _minHeapPrQ = new();
+
+    public void BuildTree(Dictionary<char, int> symbolFrequency)
+    {
+        foreach (KeyValuePair<char, int> entry in symbolFrequency)
+        {
+            MinHeapNode node = new MinHeapNode(entry.Key, entry.Value, null, null);
+            _minHeapPrQ.Enqueue(node);
+        }
+
+        while (_minHeapPrQ.Count() > 1)
+        {
+            MinHeapNode left = _minHeapPrQ.Dequeue();
+            MinHeapNode right = _minHeapPrQ.Dequeue();
+            
+            MinHeapNode top = new MinHeapNode('$', left.Freq + right.Freq, null, null);
+            top.Left = left;
+            top.Right = right;
+            _minHeapPrQ.Enqueue(top);
+        }
+    }
 }
