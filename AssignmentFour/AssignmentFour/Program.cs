239d93a14fe288552b63a310a9a53188ddda3177
Kira Zholtikova <kzholtikova@kse.org.ua>
2024-03-18 20:41:22 +0200

    revised program structure

--- AssignmentFour/AssignmentFour/Program.cs
+++ AssignmentFour/AssignmentFour/Program.cs
@@ -4,41 +4,21 @@ static class Program
 {
     public static void Main()
     {
-            const string filePath = "sherlock.txt";
-        
-            if (File.Exists(filePath))
-            {
-                string content = File.ReadAllText(filePath);
+        Console.Write("Choose mode (encode / decode): ");
+        string? mode = Console.ReadLine();
 
-                Dictionary<char, int> symbolFrequencies = new Dictionary<char, int>();
-                CountSymbolFrequency(content, symbolFrequencies);
-                
-                HuffmanTree huffmanTree = new HuffmanTree();
-                huffmanTree.BuildTree(symbolFrequencies);
-                
-                Dictionary<char, string> codeTable = huffmanTree.BuildCodeTableRecursive();
-                PrintDict(codeTable);
-            } else { Console.WriteLine("File was not found."); }
-    }
-        
-        private static void CountSymbolFrequency(string text, Dictionary<char, int> frequency)
+        if (mode == "encode")
         {
-            foreach (char symbol in text)
-            {
-                if (frequency.TryGetValue(symbol, out int value))
-                {
-                    frequency[symbol] = ++value;
-                } else { frequency[symbol] = 1; }
-            }
-        
-        }
-
-        private static void PrintDict<TKey, TValue>(Dictionary<TKey, TValue> dictionary) where TKey : notnull
-        {
-            foreach (KeyValuePair<TKey, TValue> entry in dictionary)
-            {
-                Console.WriteLine($"{entry.Key}, {entry.Value}");
-            }
-        }
-
+           Coder.Encode(); 
+        } else if (mode == "decode") { Coder.Decode(); }
     }
+
+    public static void PrintDict<TKey, TValue>(Dictionary<TKey, TValue> dictionary) where TKey : notnull
+    {
+        foreach (KeyValuePair<TKey, TValue> entry in dictionary)
+        {
+            Console.WriteLine($"{entry.Key}, {entry.Value}");
+        }
+    }
+
+}
