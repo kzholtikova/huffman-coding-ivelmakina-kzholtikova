14e5d841eeb9e1ae0f2a8c8dde7b43a4f75356e8
Kira Zholtikova <kzholtikova@kse.org.ua>
2024-03-20 10:48:29 +0200

    program modifications

--- AssignmentFour/AssignmentFour/Program.cs
+++ AssignmentFour/AssignmentFour/Program.cs
@@ -4,20 +4,26 @@ static class Program
 {
     public static void Main()
     {
-        Console.Write("Choose mode (encode / decode): ");
+        Console.Write("Choose mode (Encode e / Decode d): ");
         string? mode = Console.ReadLine();
 
-        if (mode == "encode")
+        while (mode != "e" && mode != "d")
+        {
+            Console.Write("Choose mode (Encode e / Decode d): ");
+            mode = Console.ReadLine();
+        }
+
+        if (mode == "e")
         {
            Coder.Encode(); 
-        } else if (mode == "decode") { Coder.Decode(); }
+        } else if (mode == "d") { Coder.Decode(); }
     }
 
     public static void PrintDict<TKey, TValue>(Dictionary<TKey, TValue> dictionary) where TKey : notnull
     {
         foreach (KeyValuePair<TKey, TValue> entry in dictionary)
         {
-            Console.WriteLine($"{entry.Key}, {entry.Value}");
+            Console.WriteLine($"{entry.Key}: {entry.Value}");
         }
     }
 
